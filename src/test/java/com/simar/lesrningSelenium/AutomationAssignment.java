22ae6b2df6044c2e7ab180458ba46d8c5d59aa16
Simarjit <simarjassal9@gmail.com>
2022-03-23 20:04:50 -0400

    failing assertion 8 & 9

--- src/test/java/com/simar/lesrningSelenium/AutomationAssignment.java
+++ src/test/java/com/simar/lesrningSelenium/AutomationAssignment.java
@@ -7,11 +7,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+@Test
 public class AutomationAssignment {
 
 	WebDriver wd;
@@ -33,7 +35,6 @@ public class AutomationAssignment {
 		wd.manage().window().maximize();
 	}
 
-	@Test
 	public void testShoppingWebPage() {
 
 		// step 1-LOGIN TO PORTAL WebElement
@@ -57,23 +58,105 @@ public class AutomationAssignment {
 		fadedShortSleeveTshirt.click();
 
 		// Step5-INCREASE QUANTITY TO 2
-		/*
-		 * 
-		 * WebElement addQuantity = wd.findElement(By.cssSelector("quantity_wanted"));
-		 * addQuantity.clear(); addQuantity.sendKeys("2");
-		 */
-		
-		//Step 6-Select Size as L
 
+		wd.switchTo().frame(0);
+		WebElement addQuantity = wd.findElement(By.cssSelector("#quantity_wanted"));
 		/*
-		 * WebElement sizeSelection = wd.findElement(By.id("group_1"));
-		 * sizeSelection.sele
+		 * Actions action = new Actions(wd);
+		 * action.moveToElement(addQuantity).build().perform();
 		 */
-		
-		//Step7-ADD TO CART
+		addQuantity.clear();
+
+		addQuantity.sendKeys("2");
+
+		// Step 6-Select Size as L
+
+		WebElement sizeSelection = wd.findElement(By.cssSelector("#group_1"));
+		Select sizeSelect = new Select(sizeSelection);
+		sizeSelect.selectByValue("3");
+
+		// Step7-CLICK ON ADD TO CART"a[title='Proceed to checkout']
+
 		WebElement addToCart = wd.findElement(By.cssSelector("#add_to_cart"));
 		addToCart.click();
+		wd.switchTo().defaultContent();
+
+		// Step 8- ASSERT SUCCESS MESSAGE
+
+		/*
+		 * WebElement successmessage=wd.findElement(By.cssSelector(".icon-ok"));
+		 * Assert.assertEquals(successmessage.getText(),
+		 * "Product successfully added to your shopping cart");
+		 */
+
+		// Step 9-ASSERT QTY & PRODUCT
+		/*
+		 * WebElement confirmQuantity =
+		 * wd.findElement(By.id("layer_cart_product_quantity"));
+		 * Assert.assertEquals(confirmQuantity.getText(), "");
+		 */
+		//product assert
+		/*
+		 * WebElement productAssert =
+		 * wd.findElement(By.cssSelector("#layer_cart_product_title"));
+		 * Assert.assertEquals(productAssert.getText(), "Faded Short Sleeve T-shirts");
+		 * // quantity assert
+		 * 
+		 * WebElement quantityAssert =
+		 * wd.findElement(By.cssSelector("##layer_cart_product_quantity"));
+		 * Assert.assertEquals(quantityAssert.getText(), "2");
+		 */
+
+		// Step 10- PROCEED TO CHECKOUT
+		WebElement checkOutClick1 = wd.findElement(By.cssSelector("a.btn.btn-default.button.button-medium"));
+		checkOutClick1.click();
+
+		// Step11- ASSERT totalPrice
+
+		WebElement totalPrice = wd.findElement(By.cssSelector("#total_price"));
+		Assert.assertEquals(totalPrice.getText(), "$35.02");
+
+		// Step 12-CLICK PROCEED TO CHECKOUT
+		WebElement checkOutClick2 = wd.findElement(By.cssSelector("a.button.btn.btn-default.standard-checkout.button-medium"));
+		checkOutClick2.click();
 		
+
+		// Step 13-Enter in TextBox
+		WebElement addMessage = wd.findElement(By.cssSelector("textarea[name = 'message']"));
+		addMessage.sendKeys("I am buying new clothes");
+
+		// Step14-CLICK ON PROCEED TO CHECKOUT
+		WebElement checkOutClick3 = wd.findElement(By.cssSelector("button[name='processAddress']"));
+		checkOutClick3.click();
+
+		// Step 15-SELECT THE CHECKBOX
+
+		// WebElement selectCheckbox = wd.findElement(By.cssSelector("p.checkbox"));
+
+		WebElement selectCheckBox = wd.findElement(By.cssSelector("#cgv"));
+		selectCheckBox.click();
+
+		// Step16- proceed to checkout
+		WebElement checkOutClick4 = wd.findElement(By.cssSelector("button[name='processCarrier']"));
+		checkOutClick4.click();
+
+		// STEP 17-CLICK ON PAY WITH WIRE
+		WebElement payByWire = wd.findElement(By.cssSelector(".bankwire"));
+		payByWire.click();
+
+		// STEP 18-ASSERT PAYMENT METHOD
+		WebElement bankWireAssertion = wd.findElement(By.cssSelector(".page-subheading"));
+
+		Assert.assertEquals(bankWireAssertion.getText(), "BANK-WIRE PAYMENT.");
+
+		// STEP 19-CLICK ON CONFIRM ORDER
+		WebElement confirmOrderClick = wd.findElement(By.cssSelector("button.button.btn.btn-default.button-medium"));
+		confirmOrderClick.click();
+
+		// STEP 20-ASSERT ORDER CONFIRMATION
+		WebElement orderConfirmAssertion = wd.findElement(By.cssSelector("p.cheque-indent"));
+		Assert.assertEquals(orderConfirmAssertion.getText(), "Your order on My Store is complete.");
+
 	}
 
 }
